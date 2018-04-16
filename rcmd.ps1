f4f49f64ec05ddabc85a928f97f09d0026df5f39
aloyouslio <35898246+aloyouslio@users.noreply.github.com>
2018-04-16 20:59:06 +0800

    invoke remote command directly with default

---
+++ rcmd.ps1
@@ -0,0 +1,26 @@
+#rcmd get-childitem
+
+param
+(
+    [Parameter(Mandatory=$True,HelpMessage="Please enter a command to run")]
+		[String]$cmd,
+		[Parameter(Mandatory=$false)]
+		[String]$user = "administrator",
+		[String]$server = "192.168.1.200",
+                [String]$pass = ""
+ )
+
+if($pass -eq "")
+{
+    $pass = Get-Content "C:\list\access.txt"
+}
+$SecPass = ConvertTo-SecureString -String $Pass -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user,$SecPass
+
+$scriptblock = $executioncontext.invokecommand.NewScriptBlock($cmd)
+$option=New-PSsessionOption -SkipCACheck -SkipCNCheck
+$sess=New-PSSession -ComputerName $server  -Credential $cred -SessionOption $option -UseSSL
+Invoke-Command -Session $sess -ScriptBlock $scriptblock
+
+Remove-PSSession -Session $sess
+
